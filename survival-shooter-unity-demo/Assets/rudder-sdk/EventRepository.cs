52883b41d65b5a60b9e915c692de11ca2ac00d4e
Dipanjan Biswas <dipanjanb@gmail.com>
2019-09-02 12:05:06 +0530

    modified EventRepository.cs to add custom serialize/deserialize class for handling Dictionary

--- survival-shooter-unity-demo/Assets/rudder-sdk/EventRepository.cs
+++ survival-shooter-unity-demo/Assets/rudder-sdk/EventRepository.cs
@@ -1,4 +1,4 @@
-﻿using com.rudderlabs.unity.library.Event;
+﻿    using com.rudderlabs.unity.library.Event;
 using System.Collections.Generic;
 using UnityEngine;
 using System;
@@ -15,6 +15,35 @@ using System.Security.Cryptography.X509Certificates;
 
 namespace com.rudderlabs.unity.library
 {
+
+    //Custom class to allow serialization of Dictionary
+    public class KeyValuePairJsonConverter : JavaScriptConverter {
+        public override object Deserialize(IDictionary<string, object> dictionary
+                                            , Type type
+                                            , JavaScriptSerializer serializer) {
+            throw new InvalidOperationException("Sorry, I do serializations only.");
+        }
+
+        public override IDictionary<string, object> Serialize(object obj, JavaScriptSerializer serializer) {
+            Dictionary<string, object> result = new Dictionary<string, object>();
+            Dictionary<string, MyClass> dictionaryInput = obj as Dictionary<string, MyClass>;
+
+            if (dictionaryInput == null) {
+                throw new InvalidOperationException("Object must be of Dictionary<string, MyClass> type.");
+            }
+
+            foreach (KeyValuePair<string, MyClass> pair in dictionaryInput)
+                result.Add(pair.Key, pair.Value);
+
+            return result;
+        }
+
+        public override IEnumerable<Type> SupportedTypes {
+            get {
+                return new ReadOnlyCollection<Type>(new Type[] { typeof(Dictionary<string, MyClass>) });
+            }
+        }
+    }
     internal class EventRepository
     {
         internal static bool loggingEnabled = true;
@@ -155,7 +184,10 @@ namespace com.rudderlabs.unity.library
                 //                                                 {
                 //                                                     NullValueHandling = NullValueHandling.Ignore
                 //                                                 });
-                string payloadString = JsonUtility.ToJson(eventPayload);
+                //string payloadString = JsonUtility.ToJson(eventPayload);
+                JavaScriptSerializer js = new JavaScriptSerializer();
+                js.RegisterConverters(new JavaScriptConverter[] { new KeyValuePairJsonConverter() });
+                string payloadString = js.Serialize(eventPayload);
                 // string payloadString = new JavaScriptSerializer().Serialize(eventPayload);
 
                 Debug.Log("EventRepository: Payload String: " + payloadString);
