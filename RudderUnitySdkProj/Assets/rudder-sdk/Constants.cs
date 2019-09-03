1acadaade089c17eefb23022613ef662aab51612
arnab <arnab@rudderlabs.com>
2019-09-03 15:31:21 +0530

    repository cleanup

--- RudderUnitySdkProj/Assets/rudder-sdk/Constants.cs
+++
@@ -1,9 +0,0 @@
-﻿using System;
-namespace com.rudderlabs.unity.library
-{
-    public static class Constants
-    {
-        public static string BASE_URL = "http://3c9e481f.ngrok.io";
-        public static int FLUSH_QUEUE_SIZE = 100;
-    }
-}
