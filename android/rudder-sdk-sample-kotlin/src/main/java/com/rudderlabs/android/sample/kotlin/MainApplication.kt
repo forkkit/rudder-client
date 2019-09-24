027744c909542beefa71410c1971ec102f09e0b2
arnab <arnab@rudderlabs.com>
2019-09-25 00:34:51 +0530

    null handling in elementbuilder

--- android/rudder-sdk-sample-kotlin/src/main/java/com/rudderlabs/android/sample/kotlin/MainApplication.kt
+++ android/rudder-sdk-sample-kotlin/src/main/java/com/rudderlabs/android/sample/kotlin/MainApplication.kt
@@ -2,17 +2,22 @@ package com.rudderlabs.android.sample.kotlin
 
 import android.app.Application
 import com.rudderlabs.android.sdk.core.RudderClient
+import com.rudderlabs.android.sdk.core.RudderConfigBuilder
 import com.rudderlabs.android.sdk.ecomm.RudderECommerceClient
 
 class MainApplication : Application() {
     companion object {
         private const val WRITE_KEY = "test_write_key"
-        private const val END_POINT_URI = "https://6bc08432.ngrok.io/hello"
+        private const val END_POINT_URI = "https://81841de4.ngrok.io/"
         lateinit var rudderEcommClient: RudderECommerceClient
     }
 
     override fun onCreate() {
         super.onCreate()
-        rudderEcommClient = RudderECommerceClient.getInstance(this, "test_write_key")
+        rudderEcommClient = RudderECommerceClient.getInstance(
+            this, "test_write_key", RudderConfigBuilder().withEndPointUri(
+                END_POINT_URI
+            ).build()
+        )
     }
 }
